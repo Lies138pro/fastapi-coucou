8cc73f45545e2e10147823333d6b80ff017cc1f7
Lies138pro <lies138pro@gmail.com>
2025-03-12 18:40:27 +0100

    Ajout de l'endpoint POST /send-message

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Message(BaseModel):
+    text: str
 
 @app.get("/")
 def read_root():
     return {"message": "Coucou"}
+
+@app.post("/send-message")
+def send_message(msg: Message):
+    return {"response": f"Tu as envoyé : {msg.text}"}
